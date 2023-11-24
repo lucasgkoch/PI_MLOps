2a872ecb4b7f0dc9d3cf16183935a5dbe60baa88
Lucas Koch <lucasgabrielkoch1997@gmail.com>
2023-11-24 01:07:43 -0300

    commit

--- functions.py
+++ functions.py
@@ -57,7 +57,7 @@ def UserForGenre(genre: str):
 
     #Se filtra el DataFrame df_exploded por el género especificado
     df_filtered_games = df_games[df_games['genres'].str.contains(genre, case=False, na=False)]
-    del df_games
+    #del df_games
     #Se elimina la columna generes para optimizar, ya que no sera necesaria
     df_filtered_games.drop(columns='genre',inplace=True)
     #Se hace un join entre df_filtered_games y df_items usando 'id' y 'item_id'
